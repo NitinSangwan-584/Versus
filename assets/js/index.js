e64063d1eda118a26a8d73bd7b724e90da3faeb2
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-02-14 10:37:04 +0530

    slider done

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,41 @@ document.getElementById('video-container').addEventListener('click', function ()
                                 frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                       </div>`;
 });
+$(".game-slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false,
+            },
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                centerMode: true,
+                variableWidth: true,
+            },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true,
+            },
+        },
+    ],
+});
